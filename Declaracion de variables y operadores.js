6cb8709249490ffb02fa4fa46ad69c147ff977f3
JaviR011 <126350198+JaviR011@users.noreply.github.com>
2024-07-09 20:11:44 -0600

    Add files via upload

---
+++ Declaracion de variables y operadores.js
@@ -0,0 +1,266 @@
+
+//imprencion en la terminal de hola mundo qewrqwerqwer
+
+console.log("hola mundo"); console.log("hola mundo"); console.log("hola mundo") 
+
+/*
+sdfwerqer
+console.log("hola mundo")
+console.log("hola mundo")
+console.log("hola mundo")
+qwerqwerqwerqwer
+*/
+
+
+//variable de tipo number
+var numero = 10.3155
+console.log(numero)
+console.log(typeof numero)
+
+
+//Variable tipo String/cadena
+let strs="84848"
+let s='77878'
+console.log(typeof s)
+console.log(typeof strs)
+console.log(s)
+console.log(strs)
+
+//variables de tipo boolean
+
+let verdadero=true
+let falso=false
+console.log(typeof verdadero)
+console.log(typeof falso)
+console.log( verdadero)
+console.log( falso)
+
+let fsad
+console.log(fsad)
+console.log(typeof fsad)
+
+let h=null
+console.log(null)
+
+
+
+//Funciones
+
+function fun(){
+    console.log(null)
+    console.log(typeof h)
+    console.log(typeof h)
+    console.log(typeof h)
+    let a=10 
+    console.log(a)
+}
+
+class animal{
+    constructor(nombre,especie){
+
+    }
+}
+
+//cadena vacia
+let cgasdf=""
+console.log(cgasdf)
+console.log(typeof cgasdf)
+
+
+//Agregando arreglos
+
+let array=[1,2,2,2,22,2,2]
+console.log(typeof array)
+
+
+//suma
+let a=5
+let b=6
+let sum= a+b
+let suma = 5+9
+console.log(suma)
+console.log(sum)
+
+
+//resta
+let a1=5
+let b1=6
+let resultado= a1-b1
+let resul = (9-5)
+console.log(resultado)
+console.log(resul)
+//multi
+var a2=5
+var b2=6
+var resul1= a2*b2
+var res1 = (5*9)
+console.log(resul1)
+console.log(res1)
+
+//division
+var a2=5
+var b2=6
+var resul1= a2/b2
+var res1 = (5/9)
+console.log(resul1)
+console.log(res1)
+
+//modulo
+var modulo = 89 % 8
+console.log(modulo)
+
+
+//potencia
+let potencia=6**7
+console.log(potencia)
+
+
+//incremento
+let g=7
+console.log(g)
+g++
+console.log(g)
+g++
+console.log(g)
+//decremento
+g--
+console.log(g)
+g--
+console.log(g)
+g--
+console.log(g)
+g--
+console.log(g)
+//asignacion
+g=23
+console.log(g)
+
+//asignacion suma
+g+=9
+console.log(g)
+g+=9
+console.log(g)
+g+=9
+console.log(g)
+//asignacion resta
+g-=5
+console.log(g)
+g-=5
+console.log(g)
+g-=5
+console.log(g)
+g-=5
+console.log(g)
+//asignacion con multi
+g*=5
+console.log(g)
+g*=5
+console.log(g)
+g*=5
+console.log(g)
+g*=5
+console.log(g)
+
+//asignacion con multi
+
+g=g/5
+console.log(g)
+g/=5
+console.log(g)
+g/=5
+console.log(g)
+g/=5
+console.log(g)
+//asignacion con modulo
+g=32
+g%=5
+console.log(g)
+//asig con potencia
+g=5
+g**=7
+
+console.log(g)
+
+//igualdad
+
+var num=59
+var y='59'
+var res1 = (num===y)
+var res = (num==y)
+console.log(res1)
+console.log(res)
+
+//desigualdad
+var num=59
+var y='59'
+
+console.log(num!==y)
+console.log(num!=y)
+
+//mayor que
+
+var num=59
+var y='500'
+console.log(num>y)
+
+//menor que
+
+var num=59
+var y=590
+console.log(num<y)
+
+//mayor o igual que
+var num=59
+var y=59
+console.log(num>=y)
+
+//menor o igual que
+var num=500
+var y=59
+console.log(num<=y)
+//&&
+
+var num=500
+var y=59
+console.log(num!=y && num<y )
+
+//||
+
+var num=500
+var y=59
+console.log(num!=y || num<y )
+
+//!
+console.log(!(num!=y || num<y))
+console.log(!(num!=y && num<y))
+
+var edad=19
+var nombre="juan"
+var mensaje= nombre + " tiene "+ edad +" años"
+
+console.log(mensaje)
+var men="${nombre}"
+
+console.log(men)
+var mensaje2= `${nombre} tine ${edad} añosasd`
+console.log(mensaje2)
+
+
+//z = 6*x*x-12*y/5-x*y+15*x-3000
+var x = 60
+var y=162.6
+var z = 6*x*x-12*y/5-x*y+15*x-3000
+console.log(z)
+
+//4x2 +6x + 4 con x=60
+var x = 60
+var rex= 4*(x**2) + 6*x +4
+console.log(rex)
+
+//2x2-3x -5
+var x = 60
+var ecuacion3 = 2*x**2 -3*x -5
+console.log(ecuacion3)
+
+var ecuacion3 = 2*x*x -3*x -5
+console.log(ecuacion3)
